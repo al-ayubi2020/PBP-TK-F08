eae2001aeed105bcd1d74a5248b37b5aaf0a243c
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-10-14 14:46:42 +0700

    login

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import index
+from .views import register
+from .views import login_user
+from .views import logout_user
 
 app_name = 'landing_page'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,4 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http.response import JsonResponse
+import datetime
 
 def index(request):
-    return render(request, 'index_landing.html')
+    isLogin = str(request.user)
+    if (isLogin == 'AnonymousUser'):
+        return render(request, 'index_landing.html', {'isLogin': False})
+    return render(request, 'index_landing.html', {'isLogin': True})
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        form = UserCreationForm(request.POST)
+        if username and password:
+            try:
+                acc = User.objects.create(username=username)
+                if acc:
+                    acc.set_password(password)
+                    acc.save()
+                    messages.success(request, 'Akun telah berhasil dibuat!')
+                    return redirect('todolist:login')
+                else:
+                    messages.success(request, 'Terjadi masalah!')
+            except:
+                messages.success(request, 'Username sudah pernah digunakan!')
+        else:
+            messages.success(request, 'Tidak boleh kosong!')
+
+    
+    context = {}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) 
+            response = HttpResponseRedirect(reverse("user_dashboard:index")) 
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, "login.html", context)
+
+@login_required(login_url='/todolist/login/')
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('landing_page:index'))
+    return response

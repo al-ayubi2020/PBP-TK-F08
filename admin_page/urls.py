a60589146cd4efe9279c5bd6b7ca3fed525faf77
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-10-21 10:07:12 +0700

    add admin login

--- admin_page/urls.py
+++ admin_page/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import index
+from .views import login_user
+from .views import logout_user
+from .views import register
 
 app_name = 'admin_page'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', login_user, name='login_user'),
+    path('register/', register, name='register'),
+    path('logout/', logout_user, name='logout_user'),
 ]

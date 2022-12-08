87c5f8a933b3449db492e2afbc88d0be31198ffa
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-12-08 11:21:04 +0700

    validator handle

--- flutter/views.py
+++ flutter/views.py
@@ -42,7 +42,7 @@ def register(request):
             except:
                 return JsonResponse({ "status": 406, "message": "Username sudah pernah digunakan." }, status=406)
         else:
-            return JsonResponse({ "status": 400, "message": "Tidak boleh kosong" }, status=400)
+            return JsonResponse({ "status": 400, "message": "username dan password boleh kosong" }, status=400)
     return JsonResponse({"status": 502, "message": "Method not allowed"}, status=502)
 
 @csrf_exempt
@@ -50,18 +50,21 @@ def login(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            auth_login(request, user)
-            if (has_role(user, superUser)):
-                return JsonResponse({ "status": 200, "message": "Successfully Logged In!", "role":'admin' }, status=200)
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                if (has_role(user, superUser)):
+                    return JsonResponse({ "status": 200, "message": "Successfully Logged In!", "role":'admin' }, status=200)
+                else:
+                    return JsonResponse({ "status": 200, "message": "Successfully Logged In!", "role":'user' }, status=200)
             else:
-                return JsonResponse({ "status": 200, "message": "Successfully Logged In!", "role":'user' }, status=200)
+                return JsonResponse({
+                "status": 401,
+                "message": "Failed to Login, check your email/password.",
+                }, status=401)
         else:
-            return JsonResponse({
-              "status": 401,
-              "message": "Failed to Login, check your email/password.",
-            }, status=401)
+            return JsonResponse({ "status": 400, "message": "username dan password tidak boleh kosong" }, status=400)
     return JsonResponse({"message": "Method not allowed", 'status':502}, status=502)
 
 @csrf_exempt

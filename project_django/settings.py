f5d9ac2335a9b21c673b1d1a1344d92ad8667888
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-12-20 12:36:19 +0700

    salah apa lagi sih

--- project_django/settings.py
+++ project_django/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://*.iyoubee.xyz','https://*.127.0.0.1','https://trashsure-django-production.up.railway.app/','https://*']
+CSRF_TRUSTED_ORIGINS = ['https://*.iyoubee.xyz','https://*.127.0.0.1','https://trashsure-django-production.up.railway.app','https://*']
 # Application definition
 
 INSTALLED_APPS = [
